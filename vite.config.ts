6d24039dcfff8d6f7f1cafa4e31498f90f43a3fa
Berg <bergenion@gmail.com>
2025-12-17 14:36:19 +0500

    Add SCSS support in Vite configuration by specifying Sass as the implementation and setting legacy API options.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as sass from 'sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Явно указываем использовать пакет sass
+        implementation: sass,
+        api: 'legacy',
+      },
+    },
+  },
 })
 
