7d2e65d7a60496715795b0d6300326e5e3b9064d
Berg <bergenion@gmail.com>
2025-12-17 14:40:02 +0500

    Remove legacy Sass implementation from Vite configuration and update package.json to include sass-embedded dependency.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as sass from 'sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // Явно указываем использовать пакет sass
-        implementation: sass,
-        api: 'legacy',
-      },
-    },
-  },
 })
 
